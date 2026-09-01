31fa4fa16e31487df607807b9fd70179ccf42acc
John Magdy Lotfy Kamel <19735243+Zorono@users.noreply.github.com>
2026-09-01 13:25:50 +0300

    a script to automate clang-format process

---
+++ clang-format.ps1
@@ -0,0 +1,4 @@
+Get-ChildItem -Path "." -Recurse -Include *.cpp,*.h,*.hpp | Where-Object { $_.FullName -NotMatch '[\\/](~?\.dev|deps|build|vcpkg_installed)[\\/]' } | ForEach-Object {
+    Write-Host "Formatting: $($_.FullName)" -ForegroundColor Cyan
+    clang-format -style=file -i "$_.FullName"
+}
